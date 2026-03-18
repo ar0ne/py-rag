0c949bd701fcc9dad894cbeab8607cd64481a0f1
ar0ne <camtbitakou@yandex.ru>
2026-03-18 17:04:38 +0530

    added naive RAG

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from dotenv import load_dotenv
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+
 
 load_dotenv()
 
@@ -31,11 +36,51 @@ vector_store = Chroma(
 ids = vector_store.add_documents(texts)
 
 
-results = vector_store.similarity_search(
-    'How many people are bitten by cats in the U.S. annually?',
-    k=2
+# results = vector_store.similarity_search(
+#     'How many people are bitten by cats in the U.S. annually?',
+#     k=2
+# )
+
+# # Print Resulting Chunks
+# for res in results:
+#     print(f"* {res.page_content} [{res.metadata}]\n\n")
+
+
+# Set Chroma Vector Store as the Retriever
+retriever = vector_store.as_retriever()
+
+llm = GoogleGenerativeAI(model="gemini-3-flash-preview")
+
+# Create Document Parsing Function to String
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+# Create the Prompt Template
+prompt_template = """Use the context provided to answer 
+the user's question below. If you do not know the answer 
+based on the context provided, tell the user that you do 
+not know the answer to their question based on the context
+provided and that you are sorry.
+
+context: {context}
+
+question: {query}
+
+answer: """
+
+
+custom_rag_prompt = PromptTemplate.from_template(prompt_template)
+
+rag_chain = (
+    {"context": retriever | format_docs, "query": RunnablePassthrough()}
+    | custom_rag_prompt
+    | llm
+    | StrOutputParser()
 )
 
-# Print Resulting Chunks
-for res in results:
-    print(f"* {res.page_content} [{res.metadata}]\n\n")
+result = rag_chain.invoke(
+  "Do you know how many people are bitten by cats in the U.S. every year?"
+)
+
+print(result)
